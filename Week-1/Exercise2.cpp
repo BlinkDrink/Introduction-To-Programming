6f25c026847f647b3e39ac8f6123ba839aa6fcc6
Petko Petkov <evrolan1404@gmail.com>
2020-10-20 18:26:01 +0300

    Add exercise2.cpp

---
+++ Week-1/Exercise2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Напишете програма, която извежда възрастта ви след като я подадете в конзолата.
+
+int main()
+{
+    int age;
+    std::cin >> age;
+    std::cout << "My age is: " << age;
+
+    return 0;
+}
